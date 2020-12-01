385e9e1c9cb2fda650a547ca984cca89eaec449c
Kyle Pykonen <jjrokkos@gmail.com>
2020-12-01 15:31:59 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.static(patht.join(__dirname, "Develop/public")));
 
 app.get("api/notes", function(err, res) {
     try {
-        
+        theNotes = fs.readFileSync("Develop/db/db.json", "utf8");
+        console.log("Natey Notetaker is here to help");
+        theNotes = JSON.parse(theNotes);
+    } catch (err) {
+        console.log("Found an error");
+        console.log(err);
     }
-})
+    res.json(theNotes);
+});
