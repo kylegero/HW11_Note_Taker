71bbb4370aadb25e2e1c0ed232b7840551b47cae
Kyle Pykonen <jjrokkos@gmail.com>
2020-12-01 16:42:34 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -59,3 +59,11 @@ app.delete("/api/notes/:id", function(req, res) {
     }
 });
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+  });
+
